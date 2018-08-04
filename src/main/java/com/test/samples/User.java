f0b814bda35a9104e203d717932cd18fdce7bf9e
raghavan chockalingam <raghavan20@gmail.com>
2018-08-04 01:16:29 +0100

    Removed setters from User

--- src/main/java/com/test/samples/User.java
+++ src/main/java/com/test/samples/User.java
@@ -4,17 +4,16 @@ public class User {
     private String id;
     private String name;
 
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public User(String id, String name) {
-
         this.id = id;
         this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
     }
 }
